6567985c8962587f9575c266e5c344e990f09119
Victor <victor.vidasoft@gmail.com>
2020-05-25 19:52:34 +0200

    Añadido servicio

---
+++ src/Services/BeerService.php
@@ -0,0 +1,25 @@
+<?php
+
+class BeerService implements BeerServiceInterface
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function getBeersList()
+    {
+        // TODO: Implement getBeersList() method.
+    }
+
+    public function searchBeers($foodFilter)
+    {
+        // TODO: Implement searchBeers() method.
+    }
+
+    public function getBeer($idBeer)
+    {
+        // TODO: Implement getBeer() method.
+    }
+}
